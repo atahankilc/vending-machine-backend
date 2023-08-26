ebd1e93f5e481eb7a19354bda9e0ef3f685280cd
Atahan Kılıç <atahan.kilic@hotmail.com>
2023-08-26 14:44:42 +0300

    vending machine backend initial commit

---
+++ src/test/java/com/example/vendingmachinebackend/VendingMachineBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.vendingmachinebackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VendingMachineBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
